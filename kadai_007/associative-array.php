ed125f311ebda5df4f58dae86b05a14b6f7f9dc7
y-sakamoto-9127 <158486236+y-sakamoto-9127@users.noreply.github.com>
2024-05-12 19:42:41 +0900

    【課題】連想配列を作って値とキーを出力しよう
    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>連想配列を作って値とキーを出力しよう</title>
+ </head>
+ 
+ <body>
+     <p>
+        <?php
+        // 連想配列に値を代入する
+        $vege =  ['name' => 'onion', 'price' => 200, 'weightr' => 160];
+
+        // 連想配列の値を出力する
+        print_r($vege);
+        ?>
+    </p>
+ </body>
+ 
+ </html>
